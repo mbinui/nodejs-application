312ca84e152c6e16bede462f25cc46c4d180ea36
mbinui <99750582+mbinui@users.noreply.github.com>
2024-01-07 21:02:16 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+  serverUrl: 'http://172.31.1.80:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.login': '2210e3008e5b7dfdddd8e24953f3d031859af877',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
